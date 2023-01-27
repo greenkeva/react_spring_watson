a153c3a4366a3ff377e3759c4487e5d562f049e2
Shekeva.Green <Shekeva.Green@ibm.com>
2023-01-27 09:27:44 -0600

    add user repo

--- src/main/java/com/keva/react_spring_watson/User/UserRepo.java
+++ src/main/java/com/keva/react_spring_watson/User/UserRepo.java
@@ -1,2 +1,9 @@
-package com.keva.react_spring_watson.User;public class UserRepo {
+package com.keva.react_spring_watson.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
 }
